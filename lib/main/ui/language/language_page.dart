fd6493d0ffde83793aa9292d773dfb01a86de26f
datkt <datkt@ringme.vn>
2025-12-18 17:14:27 +0700

    update

--- lib/main/ui/language/language_page.dart
+++ lib/main/ui/language/language_page.dart
@@ -1,5 +1,4 @@
 import 'package:cam_id/generated/app_localizations.dart';
-import 'package:cam_id/main/data/share_preference/share_preference.dart';
 import 'package:cam_id/main/ui/language/language_bloc.dart';
 import 'package:cam_id/main/ui/language/language_event.dart';
 import 'package:cam_id/res/app_colors.dart';
@@ -14,20 +13,19 @@ class LanguagePage extends StatefulWidget {
 }
 
 class _LanguagePageState extends State<LanguagePage> {
-  String currentLang = 'vi';
+  late String currentLang;
 
   @override
-  void initState() {
-    super.initState();
-    _loadLanguage();
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    currentLang = Localizations.localeOf(context).languageCode;
   }
 
-  Future<void> _loadLanguage() async {
-    final lang =
-    await SharePreferenceUtil.getString(ShareKey.KEY_CHANGE_LANGUAGE);
-    if (!mounted) return;
+  void _changeLanguage(String langCode) {
+    context.read<LanguageBloc>().add(ChangeLanguageEvent(Locale(langCode)));
+
     setState(() {
-      currentLang = lang ?? 'en';
+      currentLang = langCode;
     });
   }
 
@@ -36,7 +34,6 @@ class _LanguagePageState extends State<LanguagePage> {
     return Scaffold(
       body: Column(
         children: [
-          // HEADER
           Container(
             width: double.infinity,
             color: AppColors.colorMain,
@@ -54,9 +51,7 @@ class _LanguagePageState extends State<LanguagePage> {
                           Icons.arrow_back_ios_new,
                           color: Colors.white,
                         ),
-                        onPressed: () {
-                          context.pop();
-                        },
+                        onPressed: () => context.pop(),
                       ),
                     ),
                     Text(
@@ -81,45 +76,21 @@ class _LanguagePageState extends State<LanguagePage> {
                   title: AppLocalizations.of(context)!.english,
                   assetFlag: 'assets/icons/ic_en.png',
                   isSelected: currentLang == 'en',
-                  onTap: () {
-                    context.read<LanguageBloc>().add(
-                      const ChangeLanguageEvent(Locale('en')),
-                    );
-
-                    setState(() {
-                      currentLang = 'en';
-                    });
-                  },
+                  onTap: () => _changeLanguage('en'),
                 ),
                 const SizedBox(height: 12),
                 LanguageItemButton(
                   title: AppLocalizations.of(context)!.vietnamese,
                   assetFlag: 'assets/icons/ic_vn.png',
                   isSelected: currentLang == 'vi',
-                  onTap: () {
-                    context.read<LanguageBloc>().add(
-                      const ChangeLanguageEvent(Locale('vi')),
-                    );
-
-                    setState(() {
-                      currentLang = 'vi';
-                    });
-                  },
+                  onTap: () => _changeLanguage('vi'),
                 ),
                 const SizedBox(height: 12),
                 LanguageItemButton(
                   title: AppLocalizations.of(context)!.khmer,
                   assetFlag: 'assets/icons/ic_km.png',
                   isSelected: currentLang == 'km',
-                  onTap: () {
-                    context.read<LanguageBloc>().add(
-                      const ChangeLanguageEvent(Locale('km')),
-                    );
-
-                    setState(() {
-                      currentLang = 'km';
-                    });
-                  },
+                  onTap: () => _changeLanguage('km'),
                 ),
               ],
             ),
@@ -129,7 +100,6 @@ class _LanguagePageState extends State<LanguagePage> {
     );
   }
 }
-
 
 class LanguageItemButton extends StatelessWidget {
   final String title;
@@ -149,7 +119,7 @@ class LanguageItemButton extends StatelessWidget {
   Widget build(BuildContext context) {
     return InkWell(
       onTap: onTap,
-      borderRadius: BorderRadius.circular(8),
+      borderRadius: BorderRadius.circular(12),
       child: Container(
         padding: const EdgeInsets.symmetric(horizontal: 12, vertical: 12),
         decoration: BoxDecoration(
@@ -171,7 +141,6 @@ class LanguageItemButton extends StatelessWidget {
                 ),
               ),
             ),
-
             if (isSelected) Icon(Icons.check, color: Colors.green[800]),
           ],
         ),
