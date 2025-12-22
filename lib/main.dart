f0bf1c132fc881855d69039d345ff283c91c4371
duongnh12 <duongnh@ringme.vn>
2025-12-22 15:39:48 +0700

    update account firebasse

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,19 @@ import 'package:cam_id/app.dart';
 import 'package:cam_id/main/data/model/user_info_model.dart';
 import 'package:cam_id/main/data/share_preference/share_preference.dart';
 import 'package:cam_id/main/utils/logger.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_crashlytics/firebase_crashlytics.dart';
 import 'package:flutter/services.dart';
 import 'package:cam_id/main/utils/app_config.dart';
 
+import 'firebase_options.dart';
+
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   bool isFirstOpenApp =
       await SharePreferenceUtil.getBool(ShareKey.KEY_FIRST_OPEN_APP) ?? false;
   AppConfig.instance.isFirstOpenApp = isFirstOpenApp;
