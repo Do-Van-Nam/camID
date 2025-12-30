37d345ac50ead612e44790da9c5d6c9946b38192
giapnguyen220503 <giapnguyen220503@gmail.com>
2025-12-30 17:09:25 +0700

    update entertainment, game, ranking

--- lib/res/app_images.dart
+++ lib/res/app_images.dart
@@ -21,6 +21,46 @@ class AppImages {
   static final String icLogin = 'assets/icons/ic_login.svg';
   static final String icMessage = 'assets/icons/ic_message.svg';
   static final String icCall = 'assets/icons/ic_call.svg';
+  static final String icArmy = 'assets/icons/ic_army.svg';
+  static final String icArrowRight = 'assets/icons/ic_arrow_right.svg';
+  static final String icAvatarDefault = 'assets/icons/ic_avatar_default.svg';
+  static final String icCambodia = 'assets/icons/ic_cambodia.svg';
+  static final String icLanguage = 'assets/icons/ic_language.svg';
+  static final String icLogout = 'assets/icons/ic_logout.svg';
+  static final String icPassport = 'assets/icons/ic_passport.svg';
+  static final String icPoliceID = 'assets/icons/ic_police_id.svg';
+  static final String icQRCode = 'assets/icons/ic_qr_code.svg';
+  static final String icRejected = 'assets/icons/ic_rejected.svg';
+  static final String icVerified = 'assets/icons/ic_verified.svg';
+  static final String icVerifying = 'assets/icons/ic_verifying.svg';
+  static final String icSetting = 'assets/icons/ic_setting.svg';
+  static final String icCheckSelected = 'assets/icons/ic_check_selected.svg';
+  static final String icCheckUnselected =
+      'assets/icons/ic_check_unselected.svg';
+  static final String icArrowRightRed = 'assets/icons/ic_arrow_right_red.svg';
+  static final String icEmpty = 'assets/icons/ic_empty.svg';
+  static final String icEmptyNotification =
+      'assets/icons/ic_empty_notification.svg';
+  static final String icEmptyShowroom = 'assets/icons/ic_empty_showroom.svg';
+  static final String icError = 'assets/icons/ic_error.svg';
+
+  //-------------------------------IMAGES-------------------------------------//
+  static final String imgBGCamIDLogo = 'assets/images/bg_camid_logo.png';
+  static final String imgDrawer = 'assets/images/bg_drawer.png';
+  static final String imgHeaderProfile = 'assets/images/bg_header_profile.png';
+  static final String imgLogoDrawer = 'assets/images/bg_logo_drawer.png';
+  static final String imgCamIDLogo = 'assets/images/camid_logo.png';
+  static final String imgEn = 'assets/images/ic_en.png';
+  static final String imgKm = 'assets/images/ic_km.png';
+  static final String imgVn = 'assets/images/ic_vn.png';
+  static final String imgMetfone = 'assets/images/ic_metfone.png';
+  static final String imgMetfoneV2 = 'assets/images/ic_metfone_v2.png';
+  static final String imgListPaymentMethodTemp =
+      'assets/images/ic_list_payment_method_temp.png';
+  static final String icChatBot4 = 'assets/images/chatbot_ic.png';
+  static final String imgChatBot5 = 'assets/images/chatbot_ic2.png';
+  static final String imgBgLogin = 'assets/images/img_bg_login.png';
+  static final String imgLogoLogin = 'assets/images/img_logo_login.png';
 
   // loyalty
   static final String icAirplane = 'assets/icons/ic_airplane.svg';
